bcb15281a6bfd88f2da991d5c3e3290f4ef61b23
Abegail Zabala <abegailzabala.mcst@gmail.com>
2024-11-19 09:46:48 +0800

    Add files via upload

---
+++ lecture-03-setting-up-dev-environment/jdbc-test/src/JdbcTest.java
@@ -0,0 +1,51 @@
+import java.sql.*;
+
+/**
+ * 
+ * @author www.luv2code.com
+ *
+ */
+public class JdbcTest {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_almario");
+			
+			System.out.println("Database connection successful!\n");
+			
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+			
+			// 3. Execute SQL query
+			myRs = myStmt.executeQuery("select * from tb_employee_almario");
+			
+			// 4. Process the result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("Emp_LastName") + ", " + myRs.getString("Emp_FirstName"));
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+	}
+
+}
